a7e080ac0758ebc94212e3851a65bd9b4fccec37
Baxriddin Chorshanbiyev <bakhriddin.work@gmail.com>
2025-10-13 16:18:45 +0500

    test component added

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import unicorn from "eslint-plugin-unicorn";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      import: importPlugin,
+      prettier: prettierPlugin,
+      unicorn,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "es5",
+        },
+      ],
+
+      "import/no-default-export": "error",
+
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+    },
+  },
+];
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 export const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div>App</div>;
+};
+
+export default function TestComponent() {
+  return <div>Test</div>;
 }
